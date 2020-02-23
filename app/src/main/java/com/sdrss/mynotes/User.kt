21b5b9091148055e771c7a0567b5f6f826e75a2c
Shailendra <shailendraharinkhede@fitternity.com>
2020-02-23 22:17:51 +0530

    1. Add network security config into project. 2. implementing api with fab on action dialog view show.

---
+++ app/src/main/java/com/sdrss/mynotes/User.kt
@@ -0,0 +1,10 @@
+package com.sdrss.mynotes
+
+data class User(
+    val id: String?,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val age: String,
+    val password: String
+)
---
+++ app/src/main/java/com/sdrss/mynotes/ui/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.sdrss.mynotes.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.sdrss.mynotes.R
+import com.sdrss.mynotes.User
+import com.sdrss.mynotes.network.NetworkHelper
+import com.sdrss.mynotes.utils.isOnline
+import com.sdrss.mynotes.utils.showNotesAlertDialog
+import com.sdrss.mynotes.utils.showVLog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var user: User? = null
+
+    private var isUpdate: Boolean = false
+    private var notesDialog: AlertDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        floatingActionButton.setOnClickListener {
+            tvData.text = ""
+            if (notesDialog == null)
+                notesDialog = showNotesAlertDialog {
+
+                    cancelable = false
+
+                    if (!isUpdate) insertData(user = user) else updateData(user = user)
+
+                    btnCloseClickListener {
+                        showVLog("Notes Dialog close icon clicked")
+                    }
+
+                    btnSubmitClickListener {
+                        showVLog("Notes Dialog done icon clicked")
+                    }
+                }
+            //  and showing
+            notesDialog?.show()
+        }
+
+        if (isOnline()) {
+            loadData() //load Data From Api
+        } else {
+
+        }
+    }
+
+    private fun loadData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = NetworkHelper.getNetworkRequest()
+            Log.d("result", result)
+            runOnUiThread {
+                tvData.text = result
+            }
+        }
+
+    }
+
+    private fun updateData(user: User?) {
+
+    }
+
+    private fun insertData(user: User?) {
+
+    }
+}
